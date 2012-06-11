8eec2036d552a4c004522d7ad040fec63a8c755a
Markus Ast <m@rkusa.st>
2012-06-11 19:30:44 +0200

    bugfixes

--- examples/todos/app.js
+++ examples/todos/app.js
@@ -14,24 +14,41 @@ root = get('/', function(app, done) {
     done()
   })
 }, function(app) {
-  // $('#new-todo').on('keypress', function(e) {
-  //   if (e.keyCode == 13) app.todos.add(new Todo({ todo: $(this).val(), isDone: false }))
-  // })
+  $('#todo-list li').on('dblclick', function() {
+    $(this).addClass('editing')
+  })
 })
 
 root.post('/todos/new', function(app, done, params, body) {
   console.log('/todos/new')
 
   var todo = new Todo({
-    id: 1,
     todo: body.todo,
     isDone: false
   })
-  todo.create()
+  todo.save()
   app.todos.add(todo)
 
   done.redirect('/')
 }, { silent: true })
+
+root.post('/todos/:id/edit', function(app, done, params, body) {
+  console.log('/todos/' + params.id + '/edit')
+
+  var search = app.todos._collection.filter(function(model) {
+    return model.id == params.id
+  })
+
+  if (search.length > 0) {
+    var model = search[0]
+    model.todo = body.todo
+    model.save()
+  }
+
+  done.redirect('/')
+}, function(app, params) {
+  $('#todo-' + params.id).removeClass('editing')
+})
 
 root.get('/todos/:id/delete', function(app, done, params) {
   console.log('/todos/' + params.id + '/delete')
